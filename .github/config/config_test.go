a1478268d36679532dca2afc947d2b0e6f407382
Darius Mejeras <damejeras@users.noreply.github.com>
2022-08-09 17:46:11 +0300

    Clean up

--- .github/config/config_test.go
+++ .github/config/config_test.go
@@ -10,15 +10,15 @@ import (
 func TestConfig(t *testing.T) {
 	require.NoError(t, os.Setenv("KUBECONFIG", "~/.kube/config"))
 	require.NoError(t, os.Setenv("LEADER_ELECTION_ENABLED", "true"))
-	require.NoError(t, os.Setenv("LEADER_ELECTION_NAMESPACE", "castai-agent"))
-	require.NoError(t, os.Setenv("LEADER_ELECTION_LOCK_NAME", "castai-cluster-controller"))
+	require.NoError(t, os.Setenv("LEADER_ELECTION_NAMESPACE", "castai-sec-agent"))
+	require.NoError(t, os.Setenv("LEADER_ELECTION_LOCK_NAME", "castai-sec-agent"))
 
 	cfg := Get()
 
 	require.Equal(t, "~/.kube/config", cfg.Kubeconfig)
 	require.Equal(t, true, cfg.LeaderElection.Enabled)
-	require.Equal(t, "castai-agent", cfg.LeaderElection.Namespace)
-	require.Equal(t, "castai-cluster-controller", cfg.LeaderElection.LockName)
+	require.Equal(t, "castai-sec-agent", cfg.LeaderElection.Namespace)
+	require.Equal(t, "castai-sec-agent", cfg.LeaderElection.LockName)
 	require.Equal(t, 25, cfg.KubeClient.QPS)
 	require.Equal(t, 150, cfg.KubeClient.Burst)
 }
