0b5b82d260c2cd1486a9531f0649760bcbf24462
anjmao <andzej@cast.ai>
2022-08-17 12:07:54 +0300

    Add initial controller

--- controller/controller.go
+++ controller/controller.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"fmt"
 	"reflect"
-	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
@@ -18,28 +18,34 @@ import (
 	"github.com/castai/sec-agent/version"
 )
 
-func New(
-	log logrus.FieldLogger,
-	f informers.SharedInformerFactory,
-	itemHandlers []ItemHandler,
-	v version.Interface,
-) *Controller {
+func New(log logrus.FieldLogger, f informers.SharedInformerFactory, v version.Interface) *Controller {
 	typeInformerMap := map[reflect.Type]cache.SharedInformer{
-		reflect.TypeOf(&corev1.Node{}):               f.Core().V1().Nodes().Informer(),
-		reflect.TypeOf(&corev1.Pod{}):                f.Core().V1().Pods().Informer(),
-		reflect.TypeOf(&corev1.Namespace{}):          f.Core().V1().Namespaces().Informer(),
-		reflect.TypeOf(&corev1.Service{}):            f.Core().V1().Services().Informer(),
-		reflect.TypeOf(&rbacv1.ClusterRoleBinding{}): f.Rbac().V1().ClusterRoleBindings().Informer(),
-		reflect.TypeOf(&appsv1.Deployment{}):         f.Apps().V1().Deployments().Informer(),
-		reflect.TypeOf(&appsv1.DaemonSet{}):          f.Apps().V1().DaemonSets().Informer(),
-		reflect.TypeOf(&appsv1.StatefulSet{}):        f.Apps().V1().StatefulSets().Informer(),
-		// TODO: Add jobs, cronjobs and other resources for kubelinter.
+		reflect.TypeOf(&corev1.Node{}):                  f.Core().V1().Nodes().Informer(),
+		reflect.TypeOf(&corev1.PersistentVolume{}):      f.Core().V1().PersistentVolumes().Informer(),
+		reflect.TypeOf(&corev1.PersistentVolumeClaim{}): f.Core().V1().PersistentVolumeClaims().Informer(),
+		reflect.TypeOf(&corev1.Namespace{}):             f.Core().V1().Namespaces().Informer(),
+		reflect.TypeOf(&corev1.Service{}):               f.Core().V1().Services().Informer(),
+		reflect.TypeOf(&rbacv1.ClusterRoleBinding{}):    f.Rbac().V1().ClusterRoleBindings().Informer(),
+		reflect.TypeOf(&rbacv1.RoleBinding{}):           f.Rbac().V1().RoleBindings().Informer(),
+		reflect.TypeOf(&appsv1.Deployment{}):            f.Apps().V1().Deployments().Informer(),
+		reflect.TypeOf(&appsv1.DaemonSet{}):             f.Apps().V1().DaemonSets().Informer(),
+		reflect.TypeOf(&appsv1.StatefulSet{}):           f.Apps().V1().StatefulSets().Informer(),
+		reflect.TypeOf(&storagev1.StorageClass{}):       f.Storage().V1().StorageClasses().Informer(),
+		// reflect.TypeOf(&batchv1.CronJob{}):              f.Batch().V1().CronJobs().Informer(), // TODO: Add jobs.
 	}
+
+	//if v.MinorInt() >= 17 {
+	//	typeInformerMap[reflect.TypeOf(&storagev1.CSINode{})] = f.Storage().V1().CSINodes().Informer()
+	//}
+	//
+	//if v.MinorInt() >= 18 {
+	//	typeInformerMap[reflect.TypeOf(&autoscalingv1.HorizontalPodAutoscaler{})] =
+	//		f.Autoscaling().V1().HorizontalPodAutoscalers().Informer()
+	//}
 
 	c := &Controller{
 		log:             log,
 		informerFactory: f,
-		itemHandlers:    itemHandlers,
 		queue:           workqueue.NewNamed("castai-sec-agent"),
 		informers:       typeInformerMap,
 	}
@@ -51,7 +57,6 @@ func New(
 type Controller struct {
 	log             logrus.FieldLogger
 	informerFactory informers.SharedInformerFactory
-	itemHandlers    []ItemHandler
 	queue           workqueue.Interface
 	informers       map[reflect.Type]cache.SharedInformer
 }
@@ -97,15 +102,12 @@ func (c *Controller) pollQueueUntilShutdown() {
 func (c *Controller) processItem(i interface{}) {
 	defer c.queue.Done(i)
 
-	item, ok := i.(*Item)
+	item, ok := i.(*item)
 	if !ok {
-		c.log.Errorf("queue Item is not of type *Item")
+		c.log.Errorf("queue item is not of type *item")
 	}
-	c.log.Infof("processing item %v: %s", item.ObjectKey(), item.Event)
 
-	for _, handler := range c.itemHandlers {
-		handler.Handle(item)
-	}
+	c.log.Infof("processing item %v: %s", reflect.TypeOf(item.obj), item.event)
 }
 
 func (c *Controller) registerEventHandlers() {
@@ -121,40 +123,40 @@ func (c *Controller) registerEventHandlers() {
 func (c *Controller) createEventHandlers(log logrus.FieldLogger, typ reflect.Type) cache.ResourceEventHandler {
 	return cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			c.deletedUnknownHandler(log, EventAdd, obj, func(log logrus.FieldLogger, e Event, obj interface{}) {
+			c.deletedUnknownHandler(log, eventAdd, obj, func(log logrus.FieldLogger, e event, obj interface{}) {
 				c.genericHandler(log, typ, e, obj)
 			})
 		},
 		UpdateFunc: func(oldObj, newObj interface{}) {
-			c.deletedUnknownHandler(log, EventUpdate, newObj, func(log logrus.FieldLogger, e Event, obj interface{}) {
+			c.deletedUnknownHandler(log, eventUpdate, newObj, func(log logrus.FieldLogger, e event, obj interface{}) {
 				c.genericHandler(log, typ, e, obj)
 			})
 		},
 		DeleteFunc: func(obj interface{}) {
-			c.deletedUnknownHandler(log, EventDelete, obj, func(log logrus.FieldLogger, e Event, obj interface{}) {
+			c.deletedUnknownHandler(log, eventDelete, obj, func(log logrus.FieldLogger, e event, obj interface{}) {
 				c.genericHandler(log, typ, e, obj)
 			})
 		},
 	}
 }
 
-type handlerFunc func(log logrus.FieldLogger, event Event, obj interface{})
+type handlerFunc func(log logrus.FieldLogger, event event, obj interface{})
 
-// deletedUnknownHandler is used to handle cache.DeletedFinalStateUnknown where an Object was deleted but the watch
-// deletion Event was missed while disconnected from the api-server.
-func (c *Controller) deletedUnknownHandler(log logrus.FieldLogger, e Event, obj interface{}, next handlerFunc) {
+// deletedUnknownHandler is used to handle cache.DeletedFinalStateUnknown where an object was deleted but the watch
+// deletion event was missed while disconnected from the api-server.
+func (c *Controller) deletedUnknownHandler(log logrus.FieldLogger, e event, obj interface{}, next handlerFunc) {
 	if deleted, ok := obj.(cache.DeletedFinalStateUnknown); ok {
-		next(log, EventDelete, deleted.Obj)
+		next(log, eventDelete, deleted.Obj)
 	} else {
 		next(log, e, obj)
 	}
 }
 
-// genericHandler is used to add an Object to the queue.
+// genericHandler is used to add an object to the queue.
 func (c *Controller) genericHandler(
 	log logrus.FieldLogger,
 	expected reflect.Type,
-	e Event,
+	e event,
 	obj interface{},
 ) {
 	if reflect.TypeOf(obj) != expected {
@@ -162,52 +164,8 @@ func (c *Controller) genericHandler(
 		return
 	}
 
-	// Map missing metadata since kubernetes client removes object kind and api version information.
-	appsV1 := "apps/v1"
-	v1 := "v1"
-	switch o := obj.(type) {
-	case *appsv1.Deployment:
-		o.Kind = "Deployment"
-		o.APIVersion = appsV1
-	case *appsv1.StatefulSet:
-		o.Kind = "StatefulSet"
-		o.APIVersion = appsV1
-	case *appsv1.DaemonSet:
-		o.Kind = "DaemonSet"
-		o.APIVersion = appsV1
-	case *corev1.Node:
-		o.Kind = "Node"
-		o.APIVersion = v1
-	case *corev1.Namespace:
-		o.Kind = "Namespace"
-		o.APIVersion = v1
-	case *corev1.Service:
-		o.Kind = "Service"
-		o.APIVersion = v1
-	case *corev1.Pod:
-		o.Kind = "Pod"
-		o.APIVersion = v1
-		// Do not process not static pods.
-		if !isStaticPod(o) {
-			return
-		}
-	case *rbacv1.ClusterRoleBinding:
-		o.Kind = "ClusterRoleBinding"
-		o.APIVersion = "rbac.authorization.k8s.io/v1"
-	default:
-		log.Error("object is not handled")
-		return
-	}
-
-	c.queue.Add(&Item{
-		Obj:   obj.(Object),
-		Event: e,
+	c.queue.Add(&item{
+		obj:   obj.(object),
+		event: e,
 	})
-}
-
-func isStaticPod(pod *corev1.Pod) bool {
-	if pod.Spec.NodeName == "" {
-		return false
-	}
-	return strings.HasSuffix(pod.ObjectMeta.Name, pod.Spec.NodeName)
 }
--- controller/types.go
+++ controller/types.go
@@ -1,41 +1,24 @@
 package controller
 
 import (
-	"fmt"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type ItemHandler interface {
-	Handle(item *Item)
-}
-
-type Event string
+type event string
 
 const (
-	EventAdd    Event = "add"
-	EventDelete Event = "delete"
-	EventUpdate Event = "update"
+	eventAdd    event = "add"
+	eventDelete event = "delete"
+	eventUpdate event = "update"
 )
 
-type Object interface {
+type object interface {
 	runtime.Object
 	metav1.Object
 }
 
-type Item struct {
-	Obj   Object
-	Event Event
-}
-
-func (i *Item) ObjectKey() string {
-	kind := i.Obj.GetObjectKind().GroupVersionKind()
-	return fmt.Sprintf(
-		"%s/%s/%s/%s",
-		kind.Version,
-		kind.Kind,
-		i.Obj.GetNamespace(),
-		i.Obj.GetName(),
-	)
+type item struct {
+	obj   object
+	event event
 }
