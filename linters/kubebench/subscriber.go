0ec8856a84f24c14a043d0e2ab70314a8a096328
matas-cast <91058680+matas-cast@users.noreply.github.com>
2022-10-06 15:40:39 +0300

    SEC-221: backoff for node job failures (#27)
    * backoff for node job failures

--- linters/kubebench/subscriber.go
+++ linters/kubebench/subscriber.go
@@ -84,27 +84,33 @@ func (s *Subscriber) Run(ctx context.Context) error {
 
 func (s *Subscriber) processCachedNodes(ctx context.Context) error {
 	nodes := s.delta.peek()
-	s.log.Infof("linting %d nodes", len(nodes))
+	ready := 0
+
 	sem := semaphore.NewWeighted(maxConcurrentJobs)
 	for _, n := range nodes {
-		node := n
+		if !n.ready() {
+			continue
+		}
+		ready++
+		job := n
 		err := sem.Acquire(ctx, 1)
 		if err != nil {
-			s.log.Errorf("kube-bench semaphore: %v", err)
-			continue
+			return fmt.Errorf("kube-bench semaphore: %w", err)
 		}
 		go func() {
 			defer sem.Release(1)
 			ctx, cancel := context.WithTimeout(ctx, nodeScanTimeout)
 			defer cancel()
-			err = s.lintNode(ctx, &node)
+			err = s.lintNode(ctx, job.node)
 			if err != nil {
-				s.log.Errorf("kube-bench: %v", err)
+				s.log.Errorf("kube-bench: %w", err)
+				job.setFailed()
 				return
 			}
-			s.delta.delete(&node)
+			s.delta.delete(job.node)
 		}()
 	}
+	s.log.Infof("linting %d nodes", ready)
 	if err := sem.Acquire(ctx, maxConcurrentJobs); err != nil {
 		return err
 	}
--- linters/kubebench/types.go
+++ linters/kubebench/types.go
@@ -2,22 +2,39 @@ package kubebench
 
 import (
 	"sync"
+	"time"
 
 	"github.com/samber/lo"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/castai/sec-agent/controller"
 )
 
 func newDeltaState() *nodeDeltaState {
 	return &nodeDeltaState{
-		objectMap: make(map[string]corev1.Node),
+		objectMap: make(map[string]*nodeJob),
 		mu:        sync.Mutex{},
 	}
 }
 
+type nodeJob struct {
+	node *corev1.Node
+
+	backoff wait.Backoff
+	next    time.Time
+}
+
+func (n *nodeJob) ready() bool {
+	return n.next.Before(time.Now())
+}
+
+func (n *nodeJob) setFailed() {
+	n.next = time.Now().Add(n.backoff.Step())
+}
+
 type nodeDeltaState struct {
-	objectMap map[string]corev1.Node
+	objectMap map[string]*nodeJob
 	mu        sync.Mutex
 }
 
@@ -26,7 +43,22 @@ func (d *nodeDeltaState) upsert(o *corev1.Node) {
 	defer d.mu.Unlock()
 
 	key := controller.ObjectKey(o)
-	d.objectMap[key] = *o
+	if job, ok := d.objectMap[key]; ok {
+		job.node = o
+		return
+	}
+
+	nodeBackoff := wait.Backoff{
+		Duration: time.Second * 15,
+		Factor:   3,
+		Steps:    8,
+	}
+	w := &nodeJob{
+		backoff: nodeBackoff,
+		node:    o,
+		next:    time.Now(),
+	}
+	d.objectMap[key] = w
 }
 
 func (d *nodeDeltaState) delete(o *corev1.Node) {
@@ -36,7 +68,7 @@ func (d *nodeDeltaState) delete(o *corev1.Node) {
 	delete(d.objectMap, controller.ObjectKey(o))
 }
 
-func (d *nodeDeltaState) peek() []corev1.Node {
+func (d *nodeDeltaState) peek() []*nodeJob {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
