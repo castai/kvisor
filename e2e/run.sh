05e834d24b57caf768b8a8f837b793e2a3284fdd
anjmao <andzej.maciusovic@gmail.com>
2023-01-06 14:12:58 +0200

    Install unchanged

--- e2e/run.sh
+++ e2e/run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-ct install --config ./ct.yaml --helm-extra-args "--set image.repository=ghcr.io/castai/kvisor/kvisor --set image.tag=${IMAGE_TAG}"
+ct install --config ./ct.yaml --all --helm-extra-args "--set image.repository=ghcr.io/castai/kvisor/kvisor --set image.tag=${IMAGE_TAG}"
