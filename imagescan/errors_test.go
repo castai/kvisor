2a2ffb99e4264ec04f1cdcffb49cbbb1a66f6a95
anjmao <andzej.maciusovic@gmail.com>
2023-11-13 18:05:51 +0200

    Fix image scan error parsing

--- imagescan/errors_test.go
+++ imagescan/errors_test.go
@@ -32,16 +32,16 @@ func TestIsPrivateImageErr(t *testing.T) {
 func TestParseErrorFromLog(t *testing.T) {
 	t.Run("PrivateImageError", func(t *testing.T) {
 		rawErr := errors.New(`time="2023-11-03T12:34:56Z" level=error msg="unauthorized: authentication required" component=image-scan`)
-		result := ParseErrorFromLog(rawErr)
-		if errors.Is(result, errPrivateImage) {
+		result := parseErrorFromLog(rawErr)
+		if !errors.Is(result, errPrivateImage) {
 			t.Errorf("Expected %v, but got %v", errPrivateImage, result)
 		}
 	})
 
 	t.Run("HostFSError", func(t *testing.T) {
 		rawErr := errors.New(`time="2023-11-03T12:34:56Z" level=error msg="no such file or directory" component=image-scan`)
-		result := ParseErrorFromLog(rawErr)
-		if errors.Is(result, errImageScanLayerNotFound) {
+		result := parseErrorFromLog(rawErr)
+		if !errors.Is(result, errImageScanLayerNotFound) {
 			t.Errorf("Expected %v, but got %v", errImageScanLayerNotFound, result)
 		}
 	})
@@ -49,28 +49,24 @@ func TestParseErrorFromLog(t *testing.T) {
 	t.Run("LogsWithErrors", func(t *testing.T) {
 		expectedErr := errors.New(`An error occurred`)
 		rawErr := errors.New(`time="2023-11-03T12:34:56Z" level=error msg="An error occurred" component=image-scan`)
-		result := ParseErrorFromLog(rawErr)
-		if errors.Is(result, expectedErr) {
-			t.Errorf("Expected 'An error occurred', but got %v", result)
-		}
+		result := parseErrorFromLog(rawErr)
+		require.EqualError(t, expectedErr, result.Error())
 	})
 
 	t.Run("LogsWithoutErrors", func(t *testing.T) {
 		rawErr := errors.New(`time="2023-11-03T12:34:56Z" level=info msg="No error occurred" component=image-scan`)
-		result := ParseErrorFromLog(rawErr)
+		result := parseErrorFromLog(rawErr)
 		if !errors.Is(result, rawErr) {
 			t.Errorf("Expected the original error, but got %v", result)
 		}
 	})
+
 	t.Run("LogWithUnknownError", func(t *testing.T) {
 		expectedErr := errors.New(`who knows which error`)
 		rawErr := errors.New(`scan job failed: time="2023-11-01T10:17:27Z" level=info msg="running image scan job, version=local, commit=undefined" component=imagescan_job
             time="2023-11-01T10:17:27Z" level=info msg="collecting artifacts for image 'test/service:todoapp-3(docker.io/test/service@sha256:a56d1aba98ea532b3031b09d118f420a03a2e3f09eb2d5d6a81d906435e864d2)', mode=remote" component=imagescan_job
             time="2023-11-01T10:17:27Z" level=fatal msg="who knows which error" component=imagescan_job`)
-		result := ParseErrorFromLog(rawErr)
-		if errors.Is(result, expectedErr) {
-			t.Errorf("Expected %v, but got %v", expectedErr, result)
-		}
+		result := parseErrorFromLog(rawErr)
+		require.EqualError(t, expectedErr, result.Error())
 	})
-
 }
