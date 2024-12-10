9417dfbe59be1119c4517aa6e45aff265ec01efb
Matas Kulkovas <91058680+matas-cast@users.noreply.github.com>
2024-12-10 14:56:42 +0200

    fix sandbox logging (#435)

--- pkg/containers/client.go
+++ pkg/containers/client.go
@@ -232,7 +232,7 @@ func (c *Client) addContainerWithCgroup(container *criapi.Container, cg *cgroup.
 	c.containersByCgroup[cg.Id] = cont
 	c.mu.Unlock()
 
-	c.log.Debugf("added container, id=%s pod=%s name=%s sandbox=%s", container.Id, podName, containerName, sandbox.Metadata.Uid)
+	c.log.Debugf("added container, id=%s pod=%s name=%s sandbox=%s", container.Id, podName, containerName, container.PodSandboxId)
 
 	go c.fireContainerCreatedListeners(cont)
 
