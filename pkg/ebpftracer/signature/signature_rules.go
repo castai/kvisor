e3c303ca25de75b21bccf151f12fe79fb4c8f296
Patrick Pichler <patrick@cast.ai>
2025-01-24 22:27:10 +0100

    Implement basic git clone detection signature
    Git clones are a surprisingly good indicator of some sort of mischievous
    behavior (besides in CI infrastructure of course). There is now
    a dedicated signature for detecting `git clone` commands. It works by
    consuming `SchedProcessExec` event, figuring out if the executed command
    was `git` and try to parse the passed arguments to detect a potential
    clone + the used repository.

--- pkg/ebpftracer/signature/signature_rules.go
+++ pkg/ebpftracer/signature/signature_rules.go
@@ -3,8 +3,9 @@ package signature
 import "github.com/castai/kvisor/pkg/logging"
 
 type DefaultSignatureConfig struct {
-	SOCKS5DetectedSignatureEnabled bool                           `json:"SOCKS5DetectedSignatureEnabled"`
-	SOCKS5DetectedSignatureConfig  SOCKS5DetectionSignatureConfig `json:"SOCKS5DetectedSignatureConfig"`
+	SOCKS5DetectedSignatureEnabled   bool                           `json:"SOCKS5DetectedSignatureEnabled"`
+	SOCKS5DetectedSignatureConfig    SOCKS5DetectionSignatureConfig `json:"SOCKS5DetectedSignatureConfig"`
+	GitCloneDetectedSignatureEnabled bool                           `json:"GitCloneSignatureEnabled"`
 }
 
 func DefaultSignatures(log *logging.Logger, cfg SignatureEngineConfig) ([]Signature, error) {
@@ -15,6 +16,10 @@ func DefaultSignatures(log *logging.Logger, cfg SignatureEngineConfig) ([]Signat
 		} else {
 			result = append(result, s)
 		}
+	}
+
+	if cfg.DefaultSignatureConfig.GitCloneDetectedSignatureEnabled {
+		result = append(result, NewGitCloneDetectedSignature(log))
 	}
 
 	return result, nil
