80ad44cd78f0af09b58859993d2176160cbc1bba
anjmao <andzej.maciusovic@gmail.com>
2025-02-13 10:53:01 +0200

    Dev scripts

---
+++ tools/localenv/pyroscope.sh
@@ -0,0 +1,5 @@
+helm upgrade --install --repo https://grafana.github.io/helm-charts \
+  pyroscope pyroscope \
+  --create-namespace -n metrics \
+  --version 1.12.0 \
+  -f ./tools/localenv/pyroscope-values.yaml
