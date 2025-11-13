288615b01413195b45ab992213b13f91ada207a4
Aleksandr Danilov <alexander.danilov@gmail.com>
2025-11-13 15:16:22 +0200

    add node configs scrapper

---
+++ cmd/controller/controllers/nodeconfigscrapper/controller.go
@@ -0,0 +1,314 @@
+package nodeconfigscrapper
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"hash/fnv"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/castai/kvisor/cmd/controller/controllers/nodeconfigscrapper/spec"
+	"github.com/castai/kvisor/cmd/controller/kube"
+	"github.com/castai/kvisor/pkg/logging"
+	"github.com/cenkalti/backoff/v5"
+	"github.com/samber/lo"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	nodeScanTimeout   = 5 * time.Minute
+	labelJobName      = "job-name"
+	maxConcurrentJobs = 1
+	componentName     = "node-config-scrapper"
+)
+
+type kubeController interface {
+	GetKvisorAgentImageDetails() (kube.ImageDetails, error)
+}
+
+type Config struct {
+	Enabled            bool          `json:"enabled"`
+	Force              bool          `json:"force"`
+	ScanInterval       time.Duration `validate:"required" json:"scanInterval"`
+	JobImagePullPolicy string        `json:"jobImagePullPolicy"`
+	CloudProvider      string        `json:"cloudProvider"`
+	JobNamespace       string        `json:"jobNamespace"`
+}
+
+func NewController(
+	log *logging.Logger,
+	client kubernetes.Interface,
+	cfg Config,
+) *Controller {
+	return &Controller{
+		log:                           log.WithField("component", componentName),
+		client:                        client,
+		cfg:                           cfg,
+		nodes:                         newDeltaState(),
+		finishedJobDeleteWaitDuration: 10 * time.Second,
+	}
+}
+
+type Controller struct {
+	log                           *logging.Logger
+	client                        kubernetes.Interface
+	cfg                           Config
+	nodes                         *nodeDeltaState
+	kubeController                kubeController
+	finishedJobDeleteWaitDuration time.Duration
+}
+
+func (c *Controller) RequiredTypes() []reflect.Type {
+	return []reflect.Type{
+		reflect.TypeOf(&corev1.Node{}),
+	}
+}
+
+func (c *Controller) OnAdd(obj kube.Object) {
+	node, ok := obj.(*corev1.Node)
+	if ok && isNodeReady(node) {
+		c.nodes.upsert(node)
+	}
+}
+
+func (c *Controller) OnUpdate(obj kube.Object) {
+	c.OnAdd(obj)
+}
+
+func (c *Controller) OnDelete(obj kube.Object) {
+	node, ok := obj.(*corev1.Node)
+	if ok {
+		c.nodes.delete(node)
+	}
+}
+
+func (c *Controller) Run(ctx context.Context) error {
+	c.log.Info("running")
+	defer c.log.Infof("stopping")
+
+	ticker := time.NewTicker(c.cfg.ScanInterval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			err := c.process(ctx)
+			if err != nil && !errors.Is(err, context.Canceled) {
+				c.log.Errorf("error scanning node configs: %v", err)
+			}
+		}
+	}
+}
+
+func (c *Controller) process(ctx context.Context) (rerr error) {
+	nodes := c.nodes.peek()
+	var nodeJobs []*nodeJob
+	for _, nodeJob := range nodes {
+		if nodeJob.ready() && len(nodeJob.node.Spec.Taints) == 0 {
+			nodeJobs = append(nodeJobs, nodeJob)
+			if len(nodeJobs) == maxConcurrentJobs {
+				break
+			}
+		}
+	}
+	if len(nodeJobs) == 0 {
+		return nil
+	}
+
+	c.log.Infof("processing node configs")
+	defer c.log.Info("processing node configs done")
+	var wg sync.WaitGroup
+	for _, n := range nodeJobs {
+		job := n
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(ctx, nodeScanTimeout)
+			defer cancel()
+			err := c.scrapNodeConfigs(ctx, job.node)
+			if err != nil {
+				c.log.WithField("node", job.node.Name).Errorf("node configs: %v", err)
+				job.setFailed()
+				return
+			}
+			// c.delta.delete(job.node)
+		}()
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func (c *Controller) RequiredInformers() []reflect.Type {
+	return []reflect.Type{reflect.TypeOf(&corev1.Node{})}
+}
+
+func (c *Controller) scrapNodeConfigs(ctx context.Context, node *corev1.Node) (rerr error) {
+	c.log.Debugf("starting config scraper job for node=%s", node.Name)
+	jobName := generateName(node.GetName())
+	err := c.deleteJob(ctx, jobName)
+	if err != nil && !k8serrors.IsNotFound(err) {
+		c.log.Errorf("can not delete job %q: %v", jobName, err)
+		return err
+	}
+
+	if err == nil {
+		err = c.waitJobDeleted(ctx, jobName)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = c.createConfigScrapperJob(ctx, node, jobName)
+
+	if c.finishedJobDeleteWaitDuration != 0 {
+		go func() {
+			// Wait some time before deleting job. This is useful for observability and e2e tests.
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(c.finishedJobDeleteWaitDuration):
+			}
+			err = c.deleteJob(ctx, jobName)
+			if err != nil {
+				c.log.Errorf("failed deleting job %s: %v", jobName, err)
+			}
+		}()
+	} else {
+		err = c.deleteJob(ctx, jobName)
+		if err != nil {
+			c.log.Errorf("failed deleting job %s: %v", jobName, err)
+		}
+	}
+
+	return nil
+}
+
+// We are interested in job pod succeeding and not the Job
+func (c *Controller) createConfigScrapperJob(ctx context.Context, node *corev1.Node, jobName string) (*corev1.Pod, error) {
+	specFn := resolveSpec(c.cfg.CloudProvider, node)
+	jobSpec := specFn(node.GetName(), jobName)
+
+	// Set image.
+	imageDetails, err := c.kubeController.GetKvisorAgentImageDetails()
+	if err != nil {
+		return nil, fmt.Errorf("kvisor image details not found: %w", err)
+	}
+	cont := jobSpec.Spec.Template.Spec.Containers[0]
+	cont.Image = imageDetails.ScannerImageName
+	cont.ImagePullPolicy = corev1.PullPolicy(c.cfg.JobImagePullPolicy)
+	jobSpec.Spec.Template.Spec.Containers[0] = cont
+	jobSpec.Spec.Template.Spec.ImagePullSecrets = imageDetails.ImagePullSecrets
+
+	job, err := c.client.BatchV1().
+		Jobs(c.cfg.JobNamespace).
+		Create(ctx, jobSpec, metav1.CreateOptions{})
+	if err != nil {
+		c.log.Errorf("can not create kube-bench scan job: %v", err)
+		return nil, err
+	}
+	selector := labels.Set{labelJobName: job.Name}
+	var kubeBenchPod *corev1.Pod
+
+	podCtx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	_, err = backoff.Retry(podCtx,
+		func() (any, error) {
+			pods, err := c.client.CoreV1().Pods(c.cfg.JobNamespace).List(ctx, metav1.ListOptions{
+				LabelSelector: selector.String(),
+			})
+			if err != nil {
+				return nil, err
+			}
+
+			if len(pods.Items) < 1 {
+				return nil, fmt.Errorf("pod not found")
+			}
+
+			kubeBenchPod = &pods.Items[0]
+
+			if kubeBenchPod.Status.Phase == corev1.PodFailed {
+				return nil, backoff.Permanent(fmt.Errorf("kube-bench failed: %s", kubeBenchPod.Status.Message))
+			}
+
+			if kubeBenchPod.Status.Phase == corev1.PodSucceeded {
+				return nil, nil
+			}
+
+			return nil, fmt.Errorf("unknown err")
+		}, backoff.WithBackOff(backoff.NewConstantBackOff(10*time.Second)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return kubeBenchPod, nil
+}
+
+func (c *Controller) deleteJob(ctx context.Context, jobName string) error {
+	return c.client.BatchV1().Jobs(c.cfg.JobNamespace).Delete(ctx, jobName, metav1.DeleteOptions{
+		GracePeriodSeconds: lo.ToPtr(int64(0)),
+		PropagationPolicy:  lo.ToPtr(metav1.DeletePropagationBackground),
+	})
+}
+
+func (c *Controller) waitJobDeleted(ctx context.Context, jobName string) error {
+	deleteCtx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+	_, err := backoff.Retry(deleteCtx,
+		func() (any, error) {
+			_, err := c.client.BatchV1().Jobs(c.cfg.JobNamespace).Get(deleteCtx, jobName, metav1.GetOptions{})
+			if err != nil {
+				if k8serrors.IsNotFound(err) {
+					return nil, nil
+				}
+
+				return nil, backoff.Permanent(err)
+			}
+
+			return nil, fmt.Errorf("job not yet deleted")
+		}, backoff.WithBackOff(backoff.NewConstantBackOff(10*time.Second)))
+
+	return err
+}
+
+func generateName(nodeName string) string {
+	h := fnv.New32a()
+	h.Write([]byte(nodeName))
+	return fmt.Sprintf("castai-%s-%d", componentName, h.Sum32())
+}
+
+func resolveSpec(provider string, node *corev1.Node) func(nodeName, jobname string) *batchv1.Job {
+	switch provider {
+	case "gke":
+		return spec.GKE
+	case "aks":
+		return spec.AKS
+	case "eks":
+		return spec.EKS
+	default:
+		labels := node.GetLabels()
+		if _, ok := labels["node-role.kubernetes.io/control-plane"]; ok {
+			return spec.Master
+		}
+
+		return spec.Node
+	}
+}
+
+func isNodeReady(n *corev1.Node) bool {
+	if len(n.Status.Conditions) == 0 {
+		return false
+	}
+	lastCondition := n.Status.Conditions[len(n.Status.Conditions)-1]
+	return lastCondition.Type == corev1.NodeReady && lastCondition.Status == corev1.ConditionTrue
+}
---
+++ cmd/linter/nodeconfigscrapper/scrapper_test.go
@@ -0,0 +1,50 @@
+package nodeconfigscrapper
+
+import (
+	"context"
+	"testing"
+
+	castaipb "github.com/castai/kvisor/api/v1/runtime"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestScrapper(t *testing.T) {
+	clientset := fake.NewClientset()
+	scrapper := NewScrapper(&mockCastaiClient{}, clientset, &mockConfigRegistry{}, "test-node")
+
+	t.Run("get node component", func(t *testing.T) {
+		r := require.New(t)
+
+		component, err := scrapper.getKubeNodeComponent(castaipb.KubeNodeComponents_COMPONENT_NAME_KUBELET)
+		r.NoError(err)
+
+		r.Equal("./config_rules/config.yaml", component.ConfigFile.Path)
+		r.Equal(castaipb.KubeNodeComponents_COMPONENT_NAME_KUBELET, component.Name)
+		r.Equal(castaipb.KubeNodeComponents_CONFIG_TYPE_AZURE_JSON, component.ConfigType)
+	})
+
+	t.Run("send report", func(t *testing.T) {
+		r := require.New(t)
+		ctx := context.Background()
+
+		report := &castaipb.KubeNodeComponents{}
+		err := scrapper.sendReport(ctx, report)
+		r.NoError(err)
+	})
+}
+
+type mockCastaiClient struct{}
+
+func (m *mockCastaiClient) KubeNodeComponentsIngest(ctx context.Context, in *castaipb.KubeNodeComponents, opts ...grpc.CallOption) (*castaipb.KubeNodeComponentsIngestResponse, error) {
+	return nil, nil
+}
+
+type mockConfigRegistry struct{}
+
+func (c *mockConfigRegistry) GetConfigs(component castaipb.KubeNodeComponents_ComponentName) []Config {
+	return []Config{
+		{Component: castaipb.KubeNodeComponents_COMPONENT_NAME_KUBELET, Path: "./config_rules/config.yaml", Type: castaipb.KubeNodeComponents_CONFIG_TYPE_AZURE_JSON},
+	}
+}
